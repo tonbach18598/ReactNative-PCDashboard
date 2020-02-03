65633e8fd18d60eb77c245049f99c145a425f281
Bach <tonbach18598@gmail.com>
2020-02-03 10:17:10 +0700

    nav

---
+++ src/screens/forget_screen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native';
+import Values from '../ultilities/values'
+import Colors from '../ultilities/colors'
+
+export default class ForgetScreen extends Component {
+    render() {
+        return (
+        <View>
+            <Text style={{color:Colors.black}}>{Values.CANCEL}</Text>
+        </View>);
+    }
+}
--- src/ultilities/routes.js
+++ src/ultilities/routes.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react'
 import SplashScreen from '../screens/splash_screen'
 import SigninScreen from '../screens/signin_screen'
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import ForgetScreen from '../screens/forget_screen'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
 
 export default class Routes {
-    static get splashRoute() { return 'splash' }
-    static get signinRoute() { return 'signin' }
-    static get forgetRoute() { return 'forget' }
-    static get dashboardRoute() { return 'dashboard' }
-    static get postRoute() { return 'post' }
-    static get editRoute() { return 'edit' }
-    static get commentRoute() { return 'comment' }
-    static get scheduleRoute() { return 'schedule' }
-    static get homeRoute() { return 'home' }
-    static get updateRoute() { return 'update' }
-    static get changeRoute() { return 'change' }
-    static get developerRoute() { return 'developer' }
-    static get userRoute() { return 'user' }
+    static get splashRoute() { return 'Splash' }
+    static get signinRoute() { return 'Signin' }
+    static get forgetRoute() { return 'Forget' }
+    static get dashboardRoute() { return 'Dashboard' }
+    static get postRoute() { return 'Post' }
+    static get editRoute() { return 'Edit' }
+    static get commentRoute() { return 'Comment' }
+    static get scheduleRoute() { return 'Schedule' }
+    static get homeRoute() { return 'Home' }
+    static get updateRoute() { return 'Update' }
+    static get changeRoute() { return 'Change' }
+    static get developerRoute() { return 'Developer' }
+    static get userRoute() { return 'User' }
+    static get signinNavigator(){ return 'SigninNavigator'}
+    static get dashboardNavigator(){ return 'DashboardNavigator'}
 }
-
-const AppNavigator = createStackNavigator(
+const SigninNavigator = createStackNavigator(
     {
-        splash: SplashScreen,
-        signin: SigninScreen,
+        Signin: SigninScreen,
+        Forget: ForgetScreen,
+    },
+    {
+        initialRouteName: Routes.signinRoute,
+        headerMode:'none'
+    }
+)
+const DashboardNavigator = createStackNavigator(
+    {
+        Splash: SplashScreen,
+        SigninNavigator: SigninNavigator,
     },
     {
         initialRouteName: Routes.splashRoute,
-        headerMode: 'none'
+    }
+)
+const AppNavigator = createSwitchNavigator(
+    {
+        Splash: SplashScreen,
+        SigninNavigator: SigninNavigator,
+        DashboardNavigator:DashboardNavigator
+    },
+    {
+        initialRouteName: Routes.splashRoute,
     }
 )
 export const AppContainer = createAppContainer(AppNavigator)
